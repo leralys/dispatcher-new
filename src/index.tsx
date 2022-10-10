87ff7c804cb1b13a378ae46e2a43a1bf1f627f68
leralys <valerialysko@gmail.com>
2022-10-10 23:46:00 +0300

    refactor(index): theme provider and installed packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/system';
+
 import App from './App';
-import { GlobalStyle } from './globalStyles';
+import { GlobalStyle, globalMuiTheme } from './globalStyles';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={globalMuiTheme}>
+        <GlobalStyle />
+        <Routes>
+          <Route path='/' element={<App />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
